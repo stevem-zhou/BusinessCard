30b9c615df09805ebb7fed31a0f6d3c28eaa357e
Steven Zhou <stevenz9@uci.edu>
2023-04-07 00:51:21 -0700

    finished styling buttons and above

--- src/components/ContactButtons.jsx
+++ src/components/ContactButtons.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./ContactButtons.css"
+import LinkedInLogo from "../images/linkedin.png"
+import EmailLogo from "../images/EmailIcon.png"
 
 export default function ContactButtons(){
     return (
         <div className="contact-section">
-            <button className="email-button"><img></img>Email</button>
-            <button className="linkedin-button"><img></img>LinkedIn</button>
+            <button className="email-button"><img src={EmailLogo}></img>Email</button>
+            <button className="linkedin-button"><img src={LinkedInLogo}></img>LinkedIn</button>
         </div>
     )
 }
